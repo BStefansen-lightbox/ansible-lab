d93778d7c37026cf3fcb405aa0e01b2a4bfa82ca
Blake Stefansen <109697058+BStefansen-lightbox@users.noreply.github.com>
2022-12-15 11:09:19 -0500

    playbook setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 docker build -t ansible-host host/.
 docker build -t ansible-worker worker/.
 
+docker rm -f ansible-host
+docker rm -f ansible-worker
+
 docker run --name ansible-worker -d -it -p 22:22 ansible-worker:latest
 docker run --name ansible-host -d -it ansible-host:latest
 
@@ -9,4 +12,4 @@ docker network create ansible-network
 docker network connect ansible-network ansible-host 
 docker network connect ansible-network ansible-worker
 
-docker exec -it ansible-host sh
+docker exec -it --user=root ansible-host sh
